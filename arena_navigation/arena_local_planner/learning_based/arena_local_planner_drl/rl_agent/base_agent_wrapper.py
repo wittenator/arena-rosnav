38ed6bf52b3b2b8b56996b4a4a4a9562078fa0e1
tuananhroman <anh.maxiking@hotmail.de>
2021-08-29 20:26:03 +0200

    comments

--- arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/rl_agent/base_agent_wrapper.py
+++ arena_navigation/arena_local_planner/learning_based/arena_local_planner_drl/rl_agent/base_agent_wrapper.py
@@ -64,6 +64,7 @@ class BaseDRLAgent(ABC):
         self._ns_robot = (
             self._ns if robot_name is None else self._ns + robot_name
         )
+        self._robot_simulation_name = robot_name
 
         self.load_hyperparameters(path=hyperparameter_path)
         robot_setting_path = os.path.join(
@@ -165,13 +166,15 @@ class BaseDRLAgent(ABC):
         if self._num_laser_beams is None:
             self._num_laser_beams = DEFAULT_NUM_LASER_BEAMS
             print(
-                f"Wasn't able to read the number of laser beams."
+                f"{self._robot_simulation_name}:"
+                "Wasn't able to read the number of laser beams."
                 "Set to default: {DEFAULT_NUM_LASER_BEAMS}"
             )
         if self._laser_range is None:
             self._laser_range = DEFAULT_LASER_RANGE
             print(
-                f"Wasn't able to read the laser range."
+                f"{self._robot_simulation_name}:"
+                "Wasn't able to read the laser range."
                 "Set to default: {DEFAULT_LASER_RANGE}"
             )
 
@@ -307,9 +310,11 @@ class BaseDRLAgent(ABC):
 
     def get_reward(self, action: np.ndarray, obs_dict: dict) -> float:
         """Calculates the reward based on the parsed observation
+        
         Args:
             action (np.ndarray):
-                Velocity commands of the agent.
+                Velocity commands of the agent\
+                in [linear velocity, angular velocity].
             obs_dict (dict):
                 Observation dictionary where each key makes up a different \
                 kind of information about the environment.
