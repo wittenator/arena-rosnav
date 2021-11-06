f4c24098c3bbd40c32051a512117bf5f5c8f4652
wittenator <wittenator@yahoo.de>
2021-11-06 20:22:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ sudo apt update
 sudo apt-get install aptitude
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 sudo aptitude update
 sudo aptitude -y install ros-${ROS_VERSION}-desktop-full
